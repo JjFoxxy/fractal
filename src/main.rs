bc283348efc0b38f434df82623dc811c9a6ac28f
Foxxy <kirill_al@bk.ru>
2024-08-15 00:17:30 +0300

    Fix coordinates transformation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use raster::{Color, Image};
 use std::ops::Range;
 
-fn image_to_real_transform(coord: f64, real_range: f64, image_size: f64, offset: f64) -> f64 {
-    coord * real_range / image_size - offset
+fn image_to_real_transform(
+    x: (i32, i32),
+    real_range: (&Range<f64>, &Range<f64>),
+    image_size: (i32, i32),
+) -> (f64, f64) {
+    let x = (x.0 as f64, x.1 as f64);
+    let image_size = (image_size.0 as f64, image_size.1 as f64);
+    (
+        real_range.0.start + x.0 * (real_range.0.end - real_range.0.start) / image_size.0,
+        real_range.1.end - x.1 * (real_range.1.end - real_range.1.start) / image_size.1,
+    )
 }
 
 fn mandelbrot_map(x: (f64, f64), x0: (f64, f64)) -> (f64, f64) {
@@ -23,20 +32,8 @@ fn belongs_to_mandelbrot(x0: (f64, f64)) -> bool {
 fn draw_mandelbrot(image: &mut Image, range_x: Range<f64>, range_y: Range<f64>) {
     for x in 0..image.width {
         for y in 0..image.height {
-            let z_n = (
-                image_to_real_transform(
-                    x as f64,
-                    range_x.end - range_x.start,
-                    image.width as f64,
-                    (range_x.end - range_x.start) / 2.,
-                ),
-                image_to_real_transform(
-                    y as f64,
-                    range_y.end - range_y.start,
-                    image.height as f64,
-                    (range_y.end - range_y.start) / 2.,
-                ),
-            );
+            let z_n =
+                image_to_real_transform((x, y), (&range_x, &range_y), (image.width, image.height));
             if belongs_to_mandelbrot(z_n) {
                 let _ = image.set_pixel(x, y, Color::rgba(0, 0, 0, 255));
             } else {
